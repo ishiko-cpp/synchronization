ee3e39f4a3e274fb86f20c6bb95c6bc261aa148e
Xavier Leclercq <xavier@leclercq.dev>
2024-10-12 12:27:28 +0100

    Fix CI (#3)

---
+++ include/Ishiko/Synchronization.hpp
@@ -0,0 +1,9 @@
+// SPDX-FileCopyrightText: 2005-2024 Xavier Leclercq
+// SPDX-License-Identifier: BSL-1.0
+
+#ifndef GUARD_ISHIKO_CPP_SYNCHRONIZATION_HPP
+#define GUARD_ISHIKO_CPP_SYNCHRONIZATION_HPP
+
+#include "Synchronization/Spinlock.hpp"
+
+#endif
