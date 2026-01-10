142489ab097a52b1a76d36a9d9a83ba4e61cc4ee
Xavier Leclercq <xavier.leclercq@needfulsoftware.com>
2026-01-10 21:32:09 +0000

    Add Barrier class

--- include/Ishiko/Synchronization/Spinlock.hpp
+++ include/Ishiko/Synchronization/Spinlock.hpp
@@ -1,4 +1,4 @@
-// SPDX-FileCopyrightText: 2005-2024 Xavier Leclercq
+// SPDX-FileCopyrightText: 2005-2026 Xavier Leclercq
 // SPDX-License-Identifier: BSL-1.0
 
 #ifndef GUARD_ISHIKO_CPP_SPINLOCK_HPP
